43a9446c0e1beccc1d11b7d6a2992a5cd33f101c
Catherine-Lambe <catherine.lambe1@ucdconnect.ie>
2024-08-04 00:39:09 +0100

    h

--- BCMprofiles_cleanUp.py
+++ BCMprofiles_cleanUp.py
@@ -431,6 +431,87 @@ class BoundGasProfile(ccl.halos.profiles.profile_base.HaloProfile):
 
         return prof
 
+class BCMProfile(ccl.halos.profiles.profile_base.HaloProfile): 
+    """Combined profile for the stellar & ejected & bound gas & cdm components (ie- The BCM Model), with the truncated Navarro-Frenk-White (NFW) profile used to calculate the density profiles of the cold dark matter (cdm) component.
+
+    $f_c + f_b + f_e + f_* = 1$ 
+    & (assuming adiabaticity) $f_b + f_e + f_* = \bar{f}_b \equiv \frac{\Omega_b}{\Omega_M}$
+
+    For cold dark matter: $f_c\ = 1 - \bar{f}_b$.
+    For the stellar component: $f_*(M)\ = A_*\ \exp{\left[ -\frac{1}{2} \left( \frac{\log_{10}(M/M_*)}{\sigma_*} \right)^2 \right]}$,
+    with default parameters of: $A_* = 0.03$, $M_* = 10^{12.5}M_{\odot} $, & $\sigma_* = 1.2$.   
+    For the bound gas: $f_b(M)\ = \frac{\bar{f}_b - f_*(M)}{1 + (M_c/M)^{\beta}} $,      
+    with default parameter of: $M_c \simeq 10^{13.5 - 14} M_{\odot}$ & $\beta \sim 0.6$. 
+    For the ejected gas: $f_e(M)\ = \bar{f}_b\ - f_b(M)\ - f_*(M)\ $.
+    
+    """
+
+    def __init__(self, cosmo, mass_def, concentration, Gamma, fourier_analytic = True, gammaRange = (3, 20), ngamma=64, qrange=(1e-4, 1e2), nq=64, limInt=(1E-3, 5E3), beta=0.6, M_c = 10**(13.5), M_star = 10**(12.5), A_star = 0.03, sigma_star = 1.2):
+        super(CombinedAllBCMProfile, self).__init__(mass_def=mass_def, concentration=concentration, Gamma=Gamma)
+        self.boundProfile = BoundGasProfile(cosmo=cosmo, mass_def=mass_def, concentration=concentration, Gamma=Gamma, gammaRange=gammaRange, ngamma=ngamma, qrange=qrange, nq=nq, limInt=limInt, beta=beta, M_c=M_c, M_star=M_star, A_star=A_star, sigma_star=sigma_star)
+        self.ejProfile = EjectedGasProfile(cosmo=cosmo, mass_def=mass_def, beta=beta, M_c=M_c, M_star=M_star, A_star=A_star, sigma_star=sigma_star)
+        self.stellProfile = StellarProfile(cosmo=cosmo, mass_def=mass_def, M_star=M_star, A_star=A_star, sigma_star=sigma_star)
+        self.cdmProfile = ccl.halos.profiles.nfw.HaloProfileNFW(mass_def=mass_def, concentration=concentration)
+
+        # do I need these?
+        self.fourier_analytic = fourier_analytic
+        if fourier_analytic is True:
+            self._fourier = self._fourier_analytic
+            
+        self._func_normQ0 = None   # General normalised bound profile (for q=0, over Gamma)
+        self._func_normQany = None
+        
+    #    self.f_bar_b = self.cosmo['Omega_b']/self.cosmo['Omega_m']
+     #   self.f_c = 1 - self.f_bar_b
+        
+#    def _f_stell(self, M):
+ #       f_stell = self.A_star * np.exp( (-1/2)* (np.log10(M / self.M_star) / self.sigma_star)**2 )
+  #      return f_stell
+
+#    def _f_bd(self, M):     #$f_b(M)\ = \frac{\bar{f}_b - f_*(M)}{1 + (M_c/M)^{\beta}} $
+ #       f_stell = self._f_stell(M)
+  #      f_b = (self.f_bar_b - f_stell) / (1 + (self.M_c / M)**self.beta )
+   #     return f_b, f_stell
+
+    def _real(self, cosmo, r, M, scale_a=1, call_interp=True, centre_pt=None):
+   #     f_bd, f_stell = self._f_bd(M)
+    #    f_ej = self.f_bar_b - f_stell - f_bd
+        
+        prof_ej = self.ejProfile._real(cosmo, r, M, scale_a) 
+        prof_bd = self.boundProfile._real(cosmo, r, M, scale_a, call_interp)
+        prof_stell = self.stellProfile._real(cosmo, r, M, scale_a, centre_pt)
+        prof_cdm = self.cdmProfile._real(self.cosmo, r, M, scale_a) 
+
+        if np.shape(M) == ():
+            prof_array = f_ej*prof_ej + f_bd*prof_bd + f_stell*prof_stell + self.f_c*prof_cdm 
+        else:
+            prof_array = np.zeros(len(M), dtype=object)
+            i = 0
+            for e, b, s in zip(f_ej, f_bd, f_stell): # should be same as: for mass in M
+                profile = e*prof_ej[i] + b*prof_bd[i] + s*prof_stell[i] + self.f_c*prof_cdm[i] 
+                prof_array[i] = profile
+                i+=1
+        return prof_array
+
+    def _fourier_analytic(self, k, M, scale_a=1):
+  #      f_bd, f_stell = self._f_bd(M)
+   #     f_ej = self.f_bar_b - f_stell - f_bd
+        
+        prof_ej = self.ejProfile._fourier(k, M, scale_a)
+        prof_bd = self.boundProfile._fourier(k, M, scale_a)
+        prof_stell = self.stellProfile._fourier(k, M, scale_a)  
+        prof_cdm = self.cdmProfile._fourier(self.cosmo, k, M, scale_a) 
+
+        if np.shape(M) == ():
+            prof_array = f_ej*prof_ej + f_bd*prof_bd + f_stell*prof_stell + self.f_c*prof_cdm 
+        else:
+            prof_array = np.zeros(len(M), dtype=object)
+            i = 0
+            for e, b, s in zip(f_ej, f_bd, f_stell): # should be same as: for mass in M
+                profile = e*prof_ej[i] + b*prof_bd[0,i] + s*prof_stell[i] + self.f_c*prof_cdm[i]
+                prof_array[i] = profile
+                i+=1
+        return prof_array
 
 class CombinedAllBCMProfile(ccl.halos.profiles.profile_base.HaloProfile): 
     """Combined profile for the stellar & ejected & bound gas & cdm components (ie- The BCM Model), with the truncated Navarro-Frenk-White (NFW) profile used to calculate the density profiles of the cold dark matter (cdm) component.
