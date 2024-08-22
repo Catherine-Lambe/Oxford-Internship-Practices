a4cfb51f9400d1b45db0cecfd445d687ec28c515
Catherine-Lambe <catherine.lambe1@ucdconnect.ie>
2024-08-22 11:05:31 +0100

    h

--- Combiner/.ipynb_checkpoints/Combiner_Class-checkpoint.py
+++ Combiner/.ipynb_checkpoints/Combiner_Class-checkpoint.py
@@ -6,6 +6,7 @@ class CombinerClass(ccl.halos.profiles.profile_base.HaloProfile):
         # prof_list = [p1, p2, p3, ...] // = [stel_prof, cdm_prof, gas_prof, ej_prof]
         self.prof_list = prof_list
         ####
+    def divide_param
 
     def update_parameters(self, **kwargs):
 
@@ -20,13 +21,13 @@ class CombinerClass(ccl.halos.profiles.profile_base.HaloProfile):
         real_list = [prof._real(cosmo, r, M, a) for prof in self.prof_list]
         # apply np.array or np.atleast_1d for this
         profile = np.sum(np.atleast_1d(real_list))
-    return profile
+        return profile
     
 
     def _fourier(self, cosmo, k, M, a): # scale_a = 1
         # use ._fourier OR .fourier (not all have options other than inbuilt)
         # could add in to the component profiles that, if fourier_analytic (etc) is called, but there is none, Then: self._fourier = self.fourier (so call CCL)
-         fourier_list = [prof._fourier(cosmo, k, M, a) for prof in self.prof_list]
+        fourier_list = [prof._fourier(cosmo, k, M, a) for prof in self.prof_list]
         # apply np.array or np.atleast_1d for this
         profile = np.sum(np.atleast_1d(fourier_list))
-    return profile
+        return profile
